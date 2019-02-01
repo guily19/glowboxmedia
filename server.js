aa9960244b14165e478901d95da5f46465bde40a
guillem.casanova <guillemcasanova@gmail.com>
2019-02-01 11:01:45 +0100

    FIX: change the IP to 0.0.0.0

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  console.log("access")
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(80, '0.0.0.0', function () {
   console.log('Example app listening on port 3000!');
 });
